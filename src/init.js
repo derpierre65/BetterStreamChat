be7d451c9bf60010f4199e43cf5d7b1c733694eb
derpierre65 <git@derpierre65.de>
2020-07-03 20:21:35 +0200

    removed color 0000FF (bad in the trovo dark mode design) (fixed #4)

--- src/init.js
+++ src/init.js
@@ -1,4 +1,4 @@
-let defaultColors = ['#FF0000', '#0000FF', '#00FF00', '#B22222', '#FF7F50', '#9ACD32', '#FF4500', '#2E8B57', '#DAA520', '#D2691E', '#5F9EA0', '#1E90FF', '#FF69B4', '#8A2BE2', '#00FF7F'];
+let defaultColors = ['#FF0000', '#00FF00', '#B22222', '#FF7F50', '#9ACD32', '#FF4500', '#2E8B57', '#DAA520', '#D2691E', '#5F9EA0', '#1E90FF', '#FF69B4', '#8A2BE2', '#00FF7F'];
 let storageType = 'sync';
 
 const Helper = {
