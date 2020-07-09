a79f18d268cd3dd99e5e8d7f5325d518ce8fdb89
Dennis Haupt <haupt@rls.de>
2020-07-09 21:31:31 +0200

    change settings tab related to active page

--- src/init.js
+++ src/init.js
@@ -765,6 +765,18 @@ const BetterStreamChat = {
 			changelogHtml += '</ul>';
 		}
 
+		// default tab to show on open settings
+		let activeTab = "general";
+
+		// is trovo?
+		if (location.hostname.toLowerCase() === 'trovo.live') {
+			activeTab = "trovo";
+		}
+		// is youtube?
+		else if (location.hostname.toLowerCase().includes('youtube.com')) {
+			activeTab = "youtube";
+		}
+
 		//<editor-fold desc="settings div">
 		let settingsDiv = document.createElement('div');
 		this.settingsDiv = settingsDiv;
@@ -773,10 +785,10 @@ const BetterStreamChat = {
 		settingsDiv.innerHTML = `<div id="status"><p></p></div><header>
 	        <ul class="nav">
 	            <li><a data-tab="about">About</a></li>
-	            <li class="active"><a data-tab="general">General</a></li>
+	            <li class="${activeTab.localeCompare("general") === 0 ? "active" : ""}"><a data-tab="general">General</a></li>
 	            <li><a data-tab="bttvSettings">BTTV</a></li>
-	            <li><a data-tab="trovoSettings">Trovo</a></li>
-	            <li><a data-tab="youtubeSettings">YouTube</a></li>
+	            <li class="${activeTab.localeCompare("trovo") === 0 ? "active" : ""}"><a data-tab="trovoSettings">Trovo</a></li>
+	            <li class="${activeTab.localeCompare("youtube") === 0 ? "active" : ""}"><a data-tab="youtubeSettings">YouTube</a></li>
 	            <li><a data-tab="changelog">Changelog</a></li>
 	            <!--<li><a data-tab="backup">Backup/Import</a></li>-->
 	        </ul>
@@ -785,7 +797,7 @@ const BetterStreamChat = {
 	    <main class="text" data-tab="about">
             soon
 		</main>
-		<main class="active" data-tab="general">
+		<main class="${activeTab === "general" ? "active" : ""}" data-tab="general">
 			${Helper.Settings.build('general')}
 		</main>
 		<main class="text" data-tab="bttvSettings">
@@ -803,10 +815,10 @@ const BetterStreamChat = {
 			<h2>Available BetterTTV emotes</h2>
 			<ul id="bttvEmoteList"></ul>
 		</main>
-		<main data-tab="trovoSettings">
+		<main class="${activeTab == "trovo" ? "active" : ""}" data-tab="trovoSettings">
 			${Helper.Settings.build('trovo')}
 		</main>
-		<main data-tab="youtubeSettings">
+		<main class="${activeTab == "youtube" ? "active" : ""}" data-tab="youtubeSettings">
 			${Helper.Settings.build('youtube')}
 		</main>
 	    <main class="text" data-tab="changelog">
@@ -1109,6 +1121,7 @@ const Trovo = {
 
 // initialization
 let initialize = async () => {
+
 	// do non settings page stuff
 	try {
 		settings = await Helper.getSettings();
